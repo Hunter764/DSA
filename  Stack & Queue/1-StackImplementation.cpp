ef339ecb6556ed1d46030bd4a83d88568384d8f4
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-07-30 12:46:47 +0530

    Add files via upload

---
+++  Stack & Queue/1-StackImplementation.cpp
@@ -0,0 +1,46 @@
+//Implementing stack using Array
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Stack{
+    int size;
+    int * arr;
+    int top;
+    public:
+        Stack(){
+            top = -1;
+            size = 1000;
+            arr = new int[size];
+        }
+        void push(int x){
+            top++;
+            arr[top] = x;
+        }
+        int pop(){
+            int x = arr[top];
+            top--;
+            return x;
+        }
+        int Top(){
+            return arr[top];
+        }
+        int Size(){
+            return top + 1;
+        }
+};
+
+int main(){
+     Stack s;
+     s.push(6);
+     s.push(6);
+     s.push(7);
+     s.pop();
+     s.Top();
+
+     cout<<"Top of stack is before deleting any element "<<s.Top()<<endl;
+     cout<<"Size of stack before deleting any element "<<s.Size()<<endl;
+     cout<<"Size of stack after deleting an element "<<s.Size()<<endl;
+     cout<<"Top of Stack after deleting an elemnt "<<s.Top()<<endl;
+     return 0;
+}
