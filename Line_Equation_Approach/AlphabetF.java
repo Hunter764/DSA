1498f3562fbd5ec27aa768bb24ed2d221f31aea8
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-08-18 23:40:07 +0530

    Add files via upload

---
+++ Line_Equation_Approach/AlphabetF.java
@@ -0,0 +1,24 @@
+public class AlphabetF {
+    public static void main(String args[]){
+        int  n = 7;
+        for(int i=0; i<=n-1; i++){
+            for(int j=0 ;j<=n-1; j++){
+                if((i==0)|| (j==0)|| ((i==3)&& j!=5 && j!=6)){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// *******
+// *      
+// *      
+// *****  
+// *      
+// *      
+// * 
