6eaa2027ca1a9e2c08a58abfa9b4c08d94070ed3
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-08-18 23:38:48 +0530

    Add files via upload

---
+++ Line_Equation_Approach/Grid.java
@@ -0,0 +1,20 @@
+public class Grid{
+    public static void main(String args[]){
+        int n=7;
+        for(int i=0; i<=n-1; i++){
+            for(int j=0; j<=n-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// *******
+// *******
+// *******
+// *******
+// *******
+// *******
+// *******
---
+++ Line_Equation_Approach/HollowTriangle.java
@@ -0,0 +1,22 @@
+public class HollowTriangle{
+    public static void main(String args[]){
+        int n=7;    
+        for(int i=0; i<=n-1; i++){
+            for(int j=0; j<=n-1;j++){
+                if(i==n/2 || i+j==n/2 || j-i==n/2){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
+
+//    *   
+//   * *  
+//  *   * 
+// *******
