23e692b75a0d537b845f4082b7662ff63156aba9
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-08-18 23:40:56 +0530

    Add files via upload

---
+++ Line_Equation_Approach/AlphabetJ.java
@@ -0,0 +1,25 @@
+public class AlphabetJ {
+    public static void main(String args[]){
+        int  n = 7;
+        for(int i=0; i<=n-1; i++){
+            for(int j=0 ;j<=n-1; j++){
+                if((i==0)|| (j==n/2) || (i-j==n/2)){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
+
+// *******
+//    *   
+//    *   
+// *  *   
+//  * *   
+//   **   
+//    * 
