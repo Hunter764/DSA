01e49a27d3f361db1f61657d52adad6a1bffef40
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-08-18 23:42:41 +0530

    Add files via upload

---
+++ Line_Equation_Approach/AlphabetV.java
@@ -0,0 +1,21 @@
+public class AlphabetV {
+    public static void main(String args[]) {
+        int n = 7;
+        for (int i = 0; i <= n - 1; i++) {
+            for (int j = 0; j <= n - 1; j++) {
+                
+                if ((i==j &&i<=n/2) || (i+j==n-1 && i<=n/2)){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// *     *
+//  *   * 
+//   * *  
+//    *
