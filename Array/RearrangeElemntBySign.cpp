98e548d70fd8019e3c0989feb71bd9e4364a5922
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-08-24 13:04:44 +0530

    RearrangeElemntBySign.cpp

--- Array/RearrangeElemntBySign.cpp
+++ Array/RearrangeElemntBySign.cpp
@@ -1,15 +1,17 @@
 //There’s an array ‘A’ of size ‘N’ with an equal number of positive and negative elements. Without altering the relative order of positive and negative elements, you must return an array of alternately positive and negative values.
-//Brute force approach
+//optimal approach
 
 // Input:
 // arr[] = {1,2,-4,-5}, N = 4
 // Output:
 // 1 -4 2 -5
 
-// In this simple approach, since the number of positive and negative elements are the same, we put positives into an array called “pos” and negatives into an array called “neg”.
-// After segregating each of the positive and negative elements, we start putting them alternatively back into array A.
-// Since the array must begin with a positive number and the start index is 0, so all the positive numbers would be placed at even indices (2*i) and negatives at the odd indices (2*i+1), where i is the index of the pos or neg array while traversing them simultaneously.
-// This approach uses O(N+N/2) of running time due to multiple traversals which we’ll try to optimize in the optimized approach given below.
+// In this optimal approach, we will try to solve the problem in a single pass and try to arrange the array elements in the correct order in that pass only.
+// We know that the resultant array must start from a positive element so we initialize the positive index as 0 and negative index as 1 and start traversing the array such that whenever we see the first positive element, it occupies the space at 0 and then posIndex increases by 2 (alternate places).
+// Similarly, when we encounter the first negative element, it occupies the position at index 1, and then each time we find a negative number, we put it on the negIndex and it increments by 2.
+// When both the negIndex and posIndex exceed the size of the array, we see that the whole array is now rearranged alternatively according to the sign.
+
+
 
 #include<iostream>
 #include<vector>
