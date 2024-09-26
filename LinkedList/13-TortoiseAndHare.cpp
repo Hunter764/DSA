19c1ff0e069d0db91265f2e2c94de6d560e6ff79
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-09-26 09:36:24 +0530

    Add files via upload

---
+++ LinkedList/13-TortoiseAndHare.cpp
@@ -0,0 +1,48 @@
+//Tortoise And Hare Algorithm to find out the middleNode of the linked List
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* findMiddle(Node* head){
+    Node* slow = head;
+    Node* fast = head;
+    while(fast != NULL && fast->next != NULL){
+        slow =slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+
+
+int main(){
+    Node * head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+    Node* middleNode = findMiddle(head);
+
+    cout<<"The middle Node is: "<<middleNode->data<<endl;
+    return 0;
+}
+
+//The middle Node is: 3
+// T.C  O(N)
+// S.C O(1)
